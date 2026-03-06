47268240999c9ca1badd8c54c450cd3d8eb5d2b6
nowadaw43-cell <nowadaw43@gmail.com>
2026-03-06 20:57:29 +0100

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:57:03.028Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-06T19:57:17.078Z
+Commit: Fix broken links in documentation
