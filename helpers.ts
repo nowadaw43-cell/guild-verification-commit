25e68286b8b7033d86244d3c1d40a53ee18fd9fc
nowadaw43-cell <nowadaw43@gmail.com>
2026-03-06 21:00:16 +0100

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T20:00:01.078Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-06T20:00:04.063Z
+Commit: Fix broken links in documentation
