56674404a9a32da8e6d43955874038b9246278fe
nowadaw43-cell <nowadaw43@gmail.com>
2026-03-06 20:58:32 +0100

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:58:08.452Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-06T19:58:19.883Z
+Commit: Fix broken links in documentation
