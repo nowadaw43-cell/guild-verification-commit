ff890437bbc9c5945454a3785a14bcf7e00fd673
nowadaw43-cell <nowadaw43@gmail.com>
2026-03-06 21:00:48 +0100

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T20:00:23.190Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-06T20:00:36.477Z
+Commit: Fix broken links in documentation
