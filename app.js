d45420983e2e18ef154152a3be5e1f2712954c75
nowadaw43-cell <nowadaw43@gmail.com>
2026-03-06 20:58:18 +0100

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:57:19.946Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-06T19:58:06.006Z
+Commit: Fix CSS styling bug on mobile devices
